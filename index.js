f7d3dc78537097fdcc71b37548c6110eeefdbd8f
Casey Manion <casey@manion.com>
2015-12-03 14:24:05 -0800

    minor clean up and variable naming

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var fs = require("fs");
+var util = require("util");
 var config = require("./lib/config.js"); // blocking lib
-var spawnSync = require("child_process").spawnSync;
-var request = require("request");
 var JSON5 = require("json5");
+
+// clean healpers
+//
+var _ = {
+  extend: util._extend,
+  format: util.format,
+  exec: require("child_process").spawnSync,
+  post: require("request").post
+};
 
 // attachments object with specific sections and colors (mod your colors here)
 var attachments = {
@@ -35,13 +43,16 @@ if (config.prev_standup) {
     var prev_standup = fs.readFileSync(config.prev_standup, {encoding: "utf8"});
 
     // TODO: try/catch ... fails use blank ?? (shouldn't fail)
-    var prev_object = JSON5.parse(prev_standup);
+    var prev_json = JSON5.parse(prev_standup);
 
     // add Today to Yesterday ~ ?? more parsing
-    prev_object.yesterday = [].concat(prev_object.yesterday, "// -----", prev_object.today);
+    prev_json.yesterday = [].concat(prev_json.yesterday, "// -----", prev_json.today);
 
-    // create new standup file
-    fs.writeFileSync(config.standup, JSON5.stringify(prev_object, null, 2) + "\n\n// vi:syntax=javascript", "utf8");
+    // default to false unless already posted
+    prev_json.live = config.standup_json ? true : false;
+
+    // create new standup file using JSON5 to unquote keys
+    fs.writeFileSync(config.standup, JSON5.stringify(prev_json, null, 2) + "\n\n// vi:syntax=javascript", "utf8");
   }
 }
 else {
@@ -50,74 +61,74 @@ else {
 }
 
 // launch editor
-spawnSync(config.editor, [].concat(config.editor_args, config.standup), {stdio: "inherit"});
+_.exec(config.editor, [].concat(config.editor_args, config.standup), {stdio: "inherit"});
 
 // parse standup file
 var standup = fs.readFileSync(config.standup, {encoding: "utf8"});
-var standup_object = {};
+var standup_json = {};
 try {
-  standup_object = JSON5.parse(standup);
+  standup_json = JSON5.parse(standup);
 }
 catch(e) {
   // if not ok - exit (gives user feed back on parse errors)
-  console.log(util.format("Error# parsing standup file :%s", e.message).replace(/:/, "'").replace(/:/, "'").replace(/#/, ":"));
+  console.log(_.format("Error# parsing standup file :%s", e.message).replace(/:/, "'").replace(/:/, "'").replace(/#/, ":"));
   process.exit(1);
 }
 
 // fix values ... make object for postMessage
-var new_message = config.extend({channel: config.channel, as_user: config.user, text: standup_object.text.join("\n")}, attachments);
+var new_standup = _.extend({channel: config.channel, as_user: config.user, text: standup_json.text.join("\n")}, attachments);
 
 // properties
 ["breakfast", "yesterday", "today", "jira", "blockers"].forEach(function(prop, i) {
   // remove commented values
-  var values = standup_object[prop]
+  var values = standup_json[prop]
     .filter(function(v) {
       return !(/^(?:#[#-]|\/\/|\/[*])/.test(v));
     })
     .map(function(v, i) {
       return v
-        .replace(/^[*][ ]/, "\u2022 ")          // bullets
-        .replace(/^[#][ .]/, "" + (i+1) + ". ") // numbers
-        .replace(/^[-][ -]/, "\u2013 ")         // en-dash
+        .replace(/^[*][ ]/, "\u2022 ") // bullets
+        .replace(/^[-][ -]/, "\u2013 ") // en-dash
+        .replace(/^[#][ .]/, _.format("%s. ", (i+1))) // numbers
       ;
     });
   // use commas for the first and line-ends for the rest
-  new_message.attachments[i].fields[0].value = values.join(i ? "\n" : ", ");
+  new_standup.attachments[i].fields[0].value = values.join(i ? "\n" : ", ");
 });
 
 // WTF ~ this took me awhile to figure out and then remember (later)!! Need to abstract this!!
-new_message.attachments = JSON.stringify(new_message.attachments);
+new_standup.attachments = JSON.stringify(new_standup.attachments);
 
 // postMessage or update or delete
 var post_url = "https://slack.com/api/chat.postMessage?token=" + config.slack_token;
-if (config.standup_json) {
-  if (standup_object.live) {
+if (config.standup_ts_json) {
+  if (standup_json.live) {
     post_url = post_url.replace(/postMessage/, "update");
-    config.extend(new_message, config.standup_json);
+    _.extend(new_standup, config.standup_ts_json);
   }
   else {
     post_url = post_url.replace(/postMessage/, "delete");
-    new_message = config.extend({}, config.standup_json);
+    new_standup = _.extend({}, config.standup_ts_json);
   }
 }
 else {
-  if (!standup_object.live) {
-    console.log("Not Sent!! Note: the 'live' property must be true.");
+  if (!standup_json.live) {
+    console.log("Standup Not Sent! Note: the 'live' property must be true.");
     process.exit(2);
   }
 }
 
 // do it
-request.post(post_url, {form: new_message}, function(err, resp, body) {
+_.post(post_url, {form: new_standup}, function(err, resp, body) {
   var response_json = JSON.parse(body);
   if (response_json.ok) {
-    if (standup_object.live) {
+    if (standup_json.live) {
       fs.writeFileSync(config.standup.replace(/\.txt$/, ".json"), JSON.stringify({ts: response_json.ts, channel: response_json.channel}), "utf8");
-      console.log("Standup " + (config.standup_json ? "Updated!" : "Sent!"));
+      console.log(_.format("Standup %s! [channel: '%s']", (config.standup_ts_json ? "Updated" : "Sent"), config.channel));
     }
-    else if (config.standup_json) {
+    else if (config.standup_ts_json) {
       fs.unlinkSync(config.standup.replace(/\.txt$/, ".json"));
-      console.log("Standup deleted!");
+      console.log(_.format("Standup Deleted! [channel: '%s']", config.channel));
     }
   }
 });
